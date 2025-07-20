dd5b65fbb25ffd85a77bff293003ce8f0878638f
unknown <vaishnaviw736@gmail.com>
2025-07-20 18:24:48 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative Ai Project',
+    version = '0.0.0',
+    author = 'Vaishnavi Wakchaure',
+    author_email = 'vaishnaviw736@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+    
+)
